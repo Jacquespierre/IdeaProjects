0fb611f68acaa45d8258aa2c130e2360f46b6434
a20santiagojfc <san_jacques@hotmail.com>
2022-04-04 20:08:47 +0200

    EjercicioCompletoInstituto

---
+++ EjercicioCompletoInstituto/src/Miembro.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+
+/**
+ *
+ * @author nuria
+ */
+public abstract class Miembro extends Persona{
+    private Date fechaIncorporacion;
+
+    public Miembro(Date fechaIncorporacion, String DNI, String nombre, int edad, String direccion) {
+        super(DNI, nombre, edad, direccion);
+        this.fechaIncorporacion = fechaIncorporacion;
+    }
+
+    public Date getFechaIncorporacion() {
+        return fechaIncorporacion;
+    }
+
+    public void setFechaIncorporacion(Date fechaIncorporacion) {
+        this.fechaIncorporacion = fechaIncorporacion;
+    }
+
+    public abstract void aplicarBonificacion();
+}
